1a238bf5b945f142ef0f21f8dac32eb5186ecdfe
NeonWizard <averagewizard13@gmail.com>
2016-06-22 22:34:03 -0600

    Updated to 2016

--- ball.py
+++ ball.py
@@ -4,7 +4,7 @@ import pygame,random,math
 class Ball():
 	#initiate class
 	def __init__(self,screenw,screenh):
-		self.radius = random.randrange(5,20)
+		self.radius = random.randrange(60,80)
 		self.screenw = screenw
 		self.screenh = screenh
 		self.x = random.randrange(self.radius+1,self.screenw-(self.radius+1))
@@ -12,6 +12,7 @@ class Ball():
 		self.dx = random.randrange(-10,10)
 		self.dy = random.randrange(-10,10)
 		self.color = pygame.Color(random.randrange(100,200),random.randrange(100,200),random.randrange(100,200))
+		self.surface = pygame.transform.scale(pygame.image.load("papa.png"), (self.radius*2, self.radius*2))
 		self.save = self.color
 		self.bounceNextTick = False
 		self.alive = True
@@ -19,7 +20,8 @@ class Ball():
 
 	#drawing the ball and updating postion
 	def draw(self,SCREEN):
-		pygame.draw.circle(SCREEN, self.color, (self.x,self.y), self.radius)
+		# pygame.draw.circle(SCREEN, self.color, (self.x,self.y), self.radius)
+		SCREEN.blit(self.surface, (self.x, self.y))
 		self.x += self.dx
 		self.y += self.dy
 		self.bounceNextTick = False
